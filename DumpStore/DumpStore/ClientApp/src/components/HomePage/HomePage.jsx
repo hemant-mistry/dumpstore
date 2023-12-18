925165705f449e208cef47ff12dfe352e69e0f58
Hemant Mistry <hemant.mistry101@gmail.com>
2023-12-18 23:17:01 +0530

    Dumpstore UI v1 completed

--- DumpStore/DumpStore/ClientApp/src/components/HomePage/HomePage.jsx
+++ DumpStore/DumpStore/ClientApp/src/components/HomePage/HomePage.jsx
@@ -23,7 +23,7 @@ export default function HomePage(){
                         </div>
                            
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Download</a>
+                        <a href="#" className="btn btn-primary"><p>Download</p></a>
                         
                     </div>
 
